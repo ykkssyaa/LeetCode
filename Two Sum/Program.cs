82f34ee1669b6f6210c8aa9b49c654618d81e7b5
ykkssyaa <mr.ender.03@mail.ru>
2022-12-20 15:26:39 +0300

    Two Sum

---
+++ Two Sum/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Two_Sum
+{
+    public class Solution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for(int j = i + 1; j < nums.Length; j++)
+                {
+                    if(nums[i] + nums[j] == target)
+                    {
+                        return new int[] { i, j };
+                    }
+                }
+            }
+            return null;
+    }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
