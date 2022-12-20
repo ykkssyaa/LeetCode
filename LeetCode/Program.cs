8756e4f84b201c92f264d7002f9d614483ca0319
ykkssyaa <mr.ender.03@mail.ru>
2022-12-20 15:25:35 +0300

    ReverseWords

---
+++ LeetCode/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Solution
+{
+
+    public string ReverseWords(string s)
+    {
+        List<string> words = new List<string>();
+
+        string str = "";
+        string word = "";
+       // int firstWordIndex = 0, lastWordIndex = 0;
+
+        for (int i = s.Length - 1; i >= 0; i--)
+        {
+            if (s[i] != ' ')
+            {
+                word += s[i];
+            }
+            else
+            {
+                words.Add(word);
+                word = "";
+            }
+        }
+        words.Add(word);
+
+        for(int i = words.Count - 1; i >= 0; i--)
+        {
+            str += words[i];
+
+            if(i != 0)
+            str += ' ';  
+        }
+
+        return str;
+    } 
+}
+
+namespace LeetCode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "God Ding";
+
+            Solution sol = new Solution();
+
+            Console.WriteLine(sol.ReverseWords(s));
+
+            Console.WriteLine(sol.ReverseWords(s) == "doG gniD");
+
+        }
+    }
+}
