b4f2f9c3e3aa34554251a3899bc165bdbbb1f291
ykkssyaa <mr.ender.03@mail.ru>
2023-06-13 00:21:35 +0300

    709. To Lower Case

--- Golang/solutions.go
+++ Golang/solutions.go
@@ -20,6 +20,61 @@ func strStr(haystack string, needle string) int {
 	return -1
 }
 
+// https://leetcode.com/problems/merge-strings-alternately/
+// 1768. Merge Strings Alternately
+func mergeAlternately(word1 string, word2 string) string {
+	var res string
+	l := min(len(word1), len(word2))
+
+	for i := 0; i < l; i++ {
+		res += string(word1[i])
+		res += string(word2[i])
+
+	}
+
+	res += word1[l:]
+	res += word2[l:]
+
+	return res
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+// https://leetcode.com/problems/find-the-difference/
+// 389. Find the Difference
+func findTheDifference(s string, t string) byte {
+	chars := make(map[uint8]int)
+
+	for i := 0; i < len(s); i++ {
+		chars[t[i]]++
+		chars[s[i]]--
+	}
+	chars[t[len(t)-1]]++
+
+	for char, value := range chars {
+		if value != 0 {
+			return char
+		}
+	}
+	return 0
+}
+
+func findTheDifferenceVer2(s string, t string) byte {
+	var res byte
+
+	for i := 0; i < len(s); i++ {
+		res += t[i]
+		res -= s[i]
+	}
+	res += t[len(t)-1]
+	return res
+}
+
 // 242. Valid Anagram
 // https://leetcode.com/problems/valid-anagram/
 func isAnagram(s string, t string) bool {
@@ -253,4 +308,22 @@ func lengthOfLastWord(s string) int {
 		}
 	}
 	return count
+}
+
+// 709. To Lower Case
+// https://leetcode.com/problems/to-lower-case/
+func toLowerCase(s string) string {
+
+	for i := 0; i < len(s); i++ {
+		if s[i] >= 41 && s[i] <= 90 {
+			if i != len(s)-1 {
+				s = s[:i] + string(s[i]|32) + s[i+1:]
+			} else {
+				s = s[:i] + string(s[i]|32)
+			}
+
+		}
+	}
+
+	return s
 }
