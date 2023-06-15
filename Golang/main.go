6554e8184b7ddd23b0c32ad32963be72715c404a
ykkssyaa <mr.ender.03@mail.ru>
2023-06-15 20:50:19 +0300

    73. Set Matrix Zeroes with gorutines

--- Golang/main.go
+++ Golang/main.go
@@ -1,5 +1,11 @@
 package main
 
+import "fmt"
+
 func main() {
 
+	matrix := [][]int{{1, 0}, {2, 4}, {3, 6}, {1, 0}}
+	setZeroes(matrix)
+
+	fmt.Println(matrix)
 }
--- Golang/mediumTasks.go
+++ Golang/mediumTasks.go
@@ -1,5 +1,7 @@
 package main
 
+import "sync"
+
 // 1041. Robot Bounded In Circle
 // https://leetcode.com/problems/robot-bounded-in-circle/
 func isRobotBounded(instructions string) bool {
@@ -39,4 +41,54 @@ func isRobotBounded(instructions string) bool {
 	}
 
 	return position[0] == 0 && position[1] == 0 || position[2] != 0
+}
+
+// 73. Set Matrix Zeroes
+// https://leetcode.com/problems/set-matrix-zeroes/
+func setZeroes(matrix [][]int) {
+
+	columns := make([]int, len(matrix[0]))
+	lines := make([]int, len(matrix))
+
+	for i := 0; i < len(matrix); i++ {
+		for j := 0; j < len(matrix[0]); j++ {
+			if matrix[i][j] == 0 {
+				columns[j] = 1
+				lines[i] = 1
+			}
+		}
+	}
+
+	var wg sync.WaitGroup
+
+	for i := 0; i < len(columns); i++ {
+		if columns[i] == 1 {
+			wg.Add(1)
+			go func(col int) {
+				zeroColumn(matrix, col)
+				wg.Done()
+			}(i)
+		}
+	}
+	for i := 0; i < len(lines); i++ {
+		if lines[i] == 1 {
+			wg.Add(1)
+			go func(line int) {
+				zeroLine(matrix, line)
+				wg.Done()
+			}(i)
+		}
+	}
+	wg.Wait()
+}
+
+func zeroColumn(matrix [][]int, column int) {
+	for i := 0; i < len(matrix); i++ {
+		matrix[i][column] = 0
+	}
+}
+func zeroLine(matrix [][]int, line int) {
+	for i := 0; i < len(matrix[line]); i++ {
+		matrix[line][i] = 0
+	}
 }
