3dda982b83f0da1e76d8be0a8d832e5b6c6bc2e7
ykkssyaa <mr.ender.03@mail.ru>
2023-06-22 13:11:53 +0300

    43. Multiply Strings

--- Golang/solutions_test.go
+++ Golang/solutions_test.go
@@ -164,13 +164,36 @@ func Test_addBinary(t *testing.T) {
 		want string
 	}{
 		{"1", args{a: "11", b: "1"}, "100"},
-		{"1", args{a: "1010", b: "1011"}, "10101"},
-		{"1", args{a: "111", b: "11"}, "1010"},
+		{"2", args{a: "1010", b: "1011"}, "10101"},
+		{"3", args{a: "111", b: "11"}, "1010"},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := addBinary(tt.args.a, tt.args.b); got != tt.want {
 				t.Errorf("addBinary() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_multiply(t *testing.T) {
+	type args struct {
+		num1 string
+		num2 string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{"1", args{"12", "4"}, "48"},
+		{"2", args{"2", "3"}, "6"},
+		{"3", args{"123", "456"}, "56088"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := multiply(tt.args.num1, tt.args.num2); got != tt.want {
+				t.Errorf("multiply() = %v, want %v", got, tt.want)
 			}
 		})
 	}
